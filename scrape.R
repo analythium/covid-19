9686c12e8d2acdfe53125762cbbd3f9ff77611fc
Peter Solymos <psolymos@gmail.com>
2020-03-26 17:16:34 -0600

    fix projection

--- scrape.R
+++ scrape.R
@@ -83,8 +83,7 @@ if (FALSE) {
         u[[i]] <- Polygons(list(Polygon(m)), x[[i]][1])
     }
     l <- SpatialPolygons(u, 1:length(z))
-    proj4string(l) <- "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"
-    #g <- as(l, "SpatialPolygonsDataFrame")
+    proj4string(l) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
     #g@data <- data.frame(area=sapply(x, "[[", 1L))
     toGeoJSON(l, "data/alberta-areas")
     tmp <- readLines("data/alberta-areas.geojson") # minify
