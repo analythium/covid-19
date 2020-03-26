e1c7c2eda07633c9f17be292f5efdffb6aee2eb9
Peter Solymos <psolymos@gmail.com>
2020-03-26 00:49:37 -0600

    save GeoJSON of AB areas

--- scrape.R
+++ scrape.R
@@ -71,7 +71,28 @@ x <- gsub("</strong>", "", x)
 x <- gsub(" case(s)", "", x, fixed=TRUE)
 x <- strsplit(x, "<br/>")
 out$areas <- list(area=sapply(x, "[[", 1L), cases=as.integer(sapply(x, "[[", 2L)))
-
+## Geo JSON: CRS EPSG3857
+if (FALSE) {
+    library(sp)
+    library(rgdal)
+    library(leafletR)
+    z <- json[[5L]]$x$calls$args[[2L]][[1L]]
+    u <- list()
+    for (i in 1:length(z)) {
+        m <- cbind(X=z[[i]][[1]]$lng[[1]], Y=z[[i]][[1]]$lat[[1]])
+        u[[i]] <- Polygons(list(Polygon(m)), x[[i]][1])
+    }
+    l <- SpatialPolygons(u, 1:length(z))
+    proj4string(l) <- "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"
+    #g <- as(l, "SpatialPolygonsDataFrame")
+    #g@data <- data.frame(area=sapply(x, "[[", 1L))
+    toGeoJSON(l, "data/alberta-areas")
+    tmp <- readLines("data/alberta-areas.geojson") # minify
+    tmp <- gsub(" ", "", tmp)
+    tmp <- paste0(tmp, collapse="")
+    writeLines(tmp, "data/alberta-areas.geojson")
+    tmp <- readOGR("data/alberta-areas.geojson")
+}
 ## Figure 6
 ## People tested for COVID-19 in Alberta by day
 cat("OK\nScraping Fig 6 ... ")
