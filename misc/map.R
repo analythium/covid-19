273394739c0ae90709304534cd158b5607509f73
Peter Solymos <psolymos@gmail.com>
2020-04-18 22:51:24 -0600

    update global data

---
+++ misc/map.R
@@ -0,0 +1,94 @@
+library(jsonlite)
+
+f0 <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
+
+d1c <- read.csv(paste0(f0, "time_series_covid19_confirmed_US.csv"),
+                stringsAsFactors = FALSE, check.names=FALSE)
+d1d <- read.csv(paste0(f0, "time_series_covid19_deaths_US.csv"),
+                stringsAsFactors = FALSE, check.names=FALSE)
+d2c <- read.csv(paste0(f0, "time_series_covid19_confirmed_global.csv"),
+                stringsAsFactors = FALSE, check.names=FALSE)
+d2d <- read.csv(paste0(f0, "time_series_covid19_deaths_global.csv"),
+                stringsAsFactors = FALSE, check.names=FALSE)
+#d2r <- read.csv(paste0(f0, "time_series_covid19_recovered_global.csv"),
+#                stringsAsFactors = FALSE, check.names=FALSE)
+
+st <- sort(unique(l1c$Province_State))
+cold <- grepl("/", colnames(d1c))
+d3c <- d3d <- matrix(NA, length(st), sum(cold))
+dimnames(d3c) <- dimnames(d3d) <- list(st, colnames(d1c)[cold])
+xy3 <- matrix(NA, length(st), 2)
+dimnames(xy3) <- list(st, c("Long", "Lat"))
+for (i in st) {
+  ii <- d1c$Province_State==i
+  d3c[i,] <- colSums(d1c[ii, cold], na.rm=TRUE)
+  d3d[i,] <- colSums(d1d[ii, cold], na.rm=TRUE)
+  xy3[i,"Long"] <- median(d1c[ii, "Long_"])
+  xy3[i,"Lat"] <- median(d1c[ii, "Lat"])
+}
+
+clast1 <- colnames(d1c)[ncol(d1c)]
+l1c <- d1c[,!cold]
+l1d <- d1d[,!cold]
+stopifnot(all(l1c$UID==l1d$UID))
+l1c$Confirmed <- d1c[,clast1]
+l1c$Deaths <- d1d[,clast1]
+
+clast2 <- colnames(d2c)[ncol(d2c)]
+l2c <- d2c[,1:4]
+l2d <- d2d[,1:4]
+#l2r <- d2r[,1:4]
+stopifnot(all(l2c$UID==l2d$UID))
+stopifnot(all(l2c$UID==l2r$UID))
+l2c$Combined_Key <- ifelse(l2c[["Province/State"]] == "",
+  l2c[["Country/Region"]],
+  paste0(l2c[["Country/Region"]], ", ", l2c[["Province/State"]]))
+l2c$Confirmed <- d2c[,clast2]
+l2c$Deaths <- d2d[,clast2]
+#l2c$Recovered <- d2r[,clast2]
+l3c <- data.frame(Combined_Key=paste0(rownames(xy3), ", US"),
+  xy3, Confirmed=d3c[,clast1], Deaths=d3d[,clast1],
+  stringsAsFactors = FALSE)
+l1 <- l1c[,c("Combined_Key","Long_", "Lat", "Confirmed", "Deaths")]
+l2 <- l2c[,c("Combined_Key","Long", "Lat", "Confirmed", "Deaths")]
+colnames(l1) <- colnames(l2)
+
+
+l12 <- rbind(l3c, l2[l2$Combined_Key != "US",])
+l12$Confirmed[l12$Confirmed < 0] <- 0
+l12$Deaths[l12$Deaths < 0] <- 0
+l12 <- l12[l12$Long != 0 & l12$Lat != 0,]
+
+q=l12#[1:10,]
+q$ConfirmedScale <- sqrt(q$Confirmed/max(q$Confirmed))
+q$ConfirmedScale[is.na(q$ConfirmedScale)] <- 0
+q$DeathsScale <- sqrt(q$Deaths/max(q$Deaths))
+q$DeathsScale[is.na(q$DeathsScale)] <- 0
+
+xy <- lapply(1:nrow(q), function(i) c(q[i,"Long"], q[i,"Lat"]))
+names(xy) <- q$Combined_Key
+
+toJSON(q)
+toJSON(xy)
+
+library(leaflet)
+
+qq <- l12
+qq$Confirmed[qq$Confirmed < 0] <- 0
+qq$Deaths[qq$Deaths < 0] <- 0
+qq$vc <- qq$Confirmed/max(qq$Confirmed)
+qq$vd <- qq$Deaths/max(qq$Deaths)
+l <- leaflet(qq) %>% addTiles() %>%
+  addCircleMarkers(
+    color="#F30",
+    radius=~8*sqrt(vd)+2,
+    stroke = FALSE, fillOpacity = 0.5)
+
+library(htmlwidgets)
+saveWidget(l, "map.html")
+
+library(maps)
+
+map("world", col="lightgrey", mar=c(0,0,0,0))
+points(Lat ~ Long, l3c)
+points(Lat ~ Long, l12, cex=sqrt(l12$Confirmed/max(l12$Confirmed)), pch=19)
--- scrape.R
+++ scrape.R
@@ -260,17 +260,110 @@ for (i in cn) {
   }
 }
 
-cat("OK\nWriting results for Canada... ")
+cat("OK\nWriting results for Canada ... ")
 dir.create("_stats/api/v1/data/canada")
 dir.create("_stats/api/v1/data/canada/regions")
 writeLines(toJSON(z),
     "_stats/api/v1/data/canada/regions/index.json")
 writeLines(toJSON(all),
     "_stats/api/v1/data/canada/index.json")
-#Canada <- z
-#Alberta <- out
-#save(Canada, Alberta, file="_stats/data/covid-19-canada.RData")
+
+
+cat("OK\nGetting global data ... ")
+f0 <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
+
+d1c <- read.csv(paste0(f0, "time_series_covid19_confirmed_US.csv"),
+                stringsAsFactors = FALSE, check.names=FALSE)
+d1d <- read.csv(paste0(f0, "time_series_covid19_deaths_US.csv"),
+                stringsAsFactors = FALSE, check.names=FALSE)
+d2c <- read.csv(paste0(f0, "time_series_covid19_confirmed_global.csv"),
+                stringsAsFactors = FALSE, check.names=FALSE)
+d2d <- read.csv(paste0(f0, "time_series_covid19_deaths_global.csv"),
+                stringsAsFactors = FALSE, check.names=FALSE)
+#d2r <- read.csv(paste0(f0, "time_series_covid19_recovered_global.csv"),
+#                stringsAsFactors = FALSE, check.names=FALSE)
+
+st <- sort(unique(l1c$Province_State))
+cold <- grepl("/", colnames(d1c))
+d3c <- d3d <- matrix(NA, length(st), sum(cold))
+dimnames(d3c) <- dimnames(d3d) <- list(st, colnames(d1c)[cold])
+xy3 <- matrix(NA, length(st), 2)
+dimnames(xy3) <- list(st, c("Long", "Lat"))
+for (i in st) {
+  ii <- d1c$Province_State==i
+  d3c[i,] <- colSums(d1c[ii, cold], na.rm=TRUE)
+  d3d[i,] <- colSums(d1d[ii, cold], na.rm=TRUE)
+  xy3[i,"Long"] <- median(d1c[ii, "Long_"])
+  xy3[i,"Lat"] <- median(d1c[ii, "Lat"])
+}
+
+clast1 <- colnames(d1c)[ncol(d1c)]
+l1c <- d1c[,!cold]
+l1d <- d1d[,!cold]
+stopifnot(all(l1c$UID==l1d$UID))
+l1c$Confirmed <- d1c[,clast1]
+l1c$Deaths <- d1d[,clast1]
+
+clast2 <- colnames(d2c)[ncol(d2c)]
+l2c <- d2c[,1:4]
+l2d <- d2d[,1:4]
+#l2r <- d2r[,1:4]
+stopifnot(all(l2c$UID==l2d$UID))
+stopifnot(all(l2c$UID==l2r$UID))
+l2c$Combined_Key <- ifelse(l2c[["Province/State"]] == "",
+  l2c[["Country/Region"]],
+  paste0(l2c[["Country/Region"]], ", ", l2c[["Province/State"]]))
+l2c$Confirmed <- d2c[,clast2]
+l2c$Deaths <- d2d[,clast2]
+#l2c$Recovered <- d2r[,clast2]
+l3c <- data.frame(Combined_Key=paste0(rownames(xy3), ", US"),
+  xy3, Confirmed=d3c[,clast1], Deaths=d3d[,clast1],
+  stringsAsFactors = FALSE)
+l1 <- l1c[,c("Combined_Key","Long_", "Lat", "Confirmed", "Deaths")]
+l2 <- l2c[,c("Combined_Key","Long", "Lat", "Confirmed", "Deaths")]
+colnames(l1) <- colnames(l2)
+
+l12 <- rbind(l3c, l2[l2$Combined_Key != "US",])
+l12$Confirmed[l12$Confirmed < 0] <- 0
+l12$Deaths[l12$Deaths < 0] <- 0
+l12 <- l12[l12$Long != 0 & l12$Lat != 0,]
+
+q=l12#[1:10,]
+q$ConfirmedScale <- sqrt(q$Confirmed/max(q$Confirmed))
+q$ConfirmedScale[is.na(q$ConfirmedScale)] <- 0
+q$DeathsScale <- sqrt(q$Deaths/max(q$Deaths))
+q$DeathsScale[is.na(q$DeathsScale)] <- 0
+
+dt1 <- as.Date(strptime(colnames(d3c), "%m/%e/%y", "UTC"))
+dt2 <- as.Date(strptime(colnames(d2c)[-(1:4)], "%m/%e/%y", "UTC"))
+stopifnot(all(dt1==dt2))
+
+tmp1 <- as.matrix(d2c[,-(1:4)])
+rownames(tmp1) <- ifelse(l2c[["Province/State"]] == "",
+  l2c[["Country/Region"]],
+  paste0(l2c[["Country/Region"]], ", ", l2c[["Province/State"]]))
+dc <- data.frame(Date=dt1,
+  cbind(t(as.matrix(d3c)), t(tmp1)),
+  check.names = FALSE)
+tmp2 <- as.matrix(d2d[,-(1:4)])
+rownames(tmp2) <- ifelse(l2d[["Province/State"]] == "",
+  l2d[["Country/Region"]],
+  paste0(l2d[["Country/Region"]], ", ", l2d[["Province/State"]]))
+dd <- data.frame(Date=dt1,
+  cbind(t(as.matrix(d3d)), t(tmp2)))
+rownames(q) <- rownames(dc) <- rownames(dd) <- NULL
+
+cat("OK\nWriting global data ... ")
+dir.create("_stats/api/v1/data/world")
+dir.create("_stats/api/v1/data/world/latest")
+writeLines(toJSON(q),
+    "_stats/api/v1/data/canada/world/latest/index.json")
+dir.create("_stats/api/v1/data/world/confirmed")
+writeLines(toJSON(dc),
+    "_stats/api/v1/data/canada/world/confirmed/index.json")
+dir.create("_stats/api/v1/data/world/deaths")
+writeLines(toJSON(dd),
+    "_stats/api/v1/data/canada/world/deaths/index.json")
+
 cat("OK\nDONE\n\n")
-
-
 
